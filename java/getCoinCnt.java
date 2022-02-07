1750e8158eb820426ae4e9cc023a97f00655fa3c
awesomedana <dana042118@gmail.com>
2022-02-07 18:28:46 +0900

    그리디 - 거스름돈

---
+++ java/getCoinCnt.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int n = 1260;
+    int[] coin_type = { 500, 100, 50, 10 };
+    int coin_cnt = 0;
+    for (int i : coin_type) {
+      coin_cnt += n / i;
+      n %= i;
+    }
+
+    System.out.println("동전의 갯수: " + coin_cnt);
+  }
+}
