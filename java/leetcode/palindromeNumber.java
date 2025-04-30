520750429ae9e994156037724ebc591f38d96e9e
awesomedana <dana042118@gmail.com>
2025-04-30 12:12:26 +0900

    Leetcode 문제풀이

---
+++ java/leetcode/palindromeNumber.java
@@ -0,0 +1,20 @@
+package java.leetcode;
+
+// https://leetcode.com/problems/palindrome-number/description/
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+public class palindromeNumber {
+  public boolean isPalindrome(int x) {
+    StringBuilder sb = new StringBuilder();
+    String []strX = String.valueOf(x).split("");
+
+    for (int i = 0; i < strX.length; i++) {
+      sb = sb.append(strX[i]);
+    }
+    String orgX = sb.toString();
+
+    String revX = new StringBuilder(orgX).reverse().toString();
+
+    return orgX.equals(revX);
+  }
+}
